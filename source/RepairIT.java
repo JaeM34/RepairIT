b3d677b38802d850ac3a3a65a04bcdfae1e04395
Jonathan <jonathanwilliams12@gmail.com>
2023-04-13 17:56:43 -0700

    Created Computer class

--- source/RepairIT.java
+++ source/RepairIT.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class RepairIT extends Application{
@@ -12,7 +13,7 @@ public class RepairIT extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         Group root = new Group();
-        Scene scene = new Scene(root,500, 500);
+        Scene scene = new Scene(root,500, 600);
         stage.setTitle("RepairIT");
         stage.setScene(scene);
         stage.show();

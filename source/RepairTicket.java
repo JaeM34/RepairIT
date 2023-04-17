54546aee0398da2259619de5f2b4cd7db5c2ba0d
JustinM3434 <justinmolina34@gmail.com>
2023-04-17 12:45:40 -0700

    Created the RepairTicket.java file to begin work o nthe REpair TIcket

--- source/RepairTicket.java
+++ source/RepairTicket.java
@@ -1,2 +1,14 @@
 public class RepairTicket {
+
+    int ID;
+    // Customer customer
+    // Computer computer
+    // Workflow[] workflows
+    String status;
+    
+    public RepairTicket() {
+        //todo random unique ID generator
+    }
+
+
 }

738557b446e0569fbdb23c760830e8f9bfff3320
Jonathan <jonathanwilliams12@gmail.com>
2023-04-25 16:23:02 -0700

    Variables, constructor, and getters added to Computer class

--- source/Computer.java
+++ source/Computer.java
@@ -1,2 +1,25 @@
 public class Computer {
+    private String computerID;
+    private String customerID;
+    private String manufacturer;
+    private String model;
+    private String serialNumber;
+    private int year;
+
+    // Computer constructor
+    public Computer(String computerID, String customerID, String manufacturer, String model, String serialNumber, int year) {
+        this.computerID = computerID;
+        this.customerID = customerID;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.year = year;
+    }
+
+    public String getComputerID() {return computerID; }
+    public String getCustomerID() {return customerID; }
+    public String getManufacturer() {return manufacturer; }
+    public String getModel() {return model; }
+    public String getSerialNumber() {return serialNumber; }
+    public int getYear() {return year; }
 }

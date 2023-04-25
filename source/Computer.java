19b4e31e29951d3026d7c1d59684a250114749b3
Jonathan <jonathanwilliams12@gmail.com>
2023-04-25 16:49:07 -0700

    Edited/added setters. getRepairTickets still needed

--- source/Computer.java
+++ source/Computer.java
@@ -6,20 +6,21 @@ public class Computer {
     private String serialNumber;
     private int year;
 
-    // Computer constructor
-    public Computer(String computerID, String customerID, String manufacturer, String model, String serialNumber, int year) {
-        this.computerID = computerID;
-        this.customerID = customerID;
-        this.manufacturer = manufacturer;
-        this.model = model;
-        this.serialNumber = serialNumber;
-        this.year = year;
-    }
-
     public String getComputerID() {return computerID; }
     public String getCustomerID() {return customerID; }
     public String getManufacturer() {return manufacturer; }
     public String getModel() {return model; }
     public String getSerialNumber() {return serialNumber; }
     public int getYear() {return year; }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public void setYear(int year) {
+        this.year = year;
+    }
+    // todo: getRepairTickets
 }

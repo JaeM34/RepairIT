ab5bd0288ff9526bf08d0d35b86ff0462487e243
tommyvtran123 <46581856+tommyvtran123@users.noreply.github.com>
2023-04-25 21:56:25 -0700

    Updated with scalability and Search button

--- source/RepairIT.java
+++ source/RepairIT.java
@@ -1,15 +1,12 @@
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -21,6 +18,7 @@ public class RepairIT extends Application {
     private static final CustomerHandler customerHandler = new CustomerHandler();
     private static final RepairTicketHandler repairticketHandler = new RepairTicketHandler();
     private static final ComputerHandler computerHandler = new ComputerHandler();
+
     public static void main(String[] args) {
         Application.launch();
     }
@@ -28,9 +26,11 @@ public class RepairIT extends Application {
     public CustomerHandler getCustomerHandler() {
         return customerHandler;
     }
+
     public RepairTicketHandler getRepairticketHandler() {
         return repairticketHandler;
     }
+
     public ComputerHandler getComputerHandler() {
         return computerHandler;
     }
@@ -40,68 +40,56 @@ public class RepairIT extends Application {
         BorderPane root = new BorderPane();
         root.setPadding(new Insets(10));
         Scene scene = new Scene(root, 1000, 600);
-        //StackPane stackPane = new StackPane();
 
+        // Create a StackPane for the rectangle and button
+        StackPane stackPane = new StackPane();
 
+        // Create a rectangle
         Rectangle rectangle = new Rectangle();
-        rectangle.setWidth(800); // Set the width of the rectangle
-        rectangle.setHeight(500); // Set the height of the rectangle
-        rectangle.setFill(Color.WHITE); // Set the fill color of the rectangle
-        rectangle.setStroke(Color.BLACK); // Set the stroke color of the rectangle
-        rectangle.setStrokeWidth(1); // Set the stroke width of the rectangle
-        rectangle.setX((scene.getWidth() - rectangle.getWidth()) / 2); // Set the X coordinate of the rectangle to center it horizontally
-        rectangle.setY((scene.getHeight() - rectangle.getHeight()) / 2); // Set the Y coordinate of the rectangle to center it vertically
+        rectangle.setFill(Color.WHITE);
+        rectangle.setStroke(Color.BLACK); // Set border color
+        rectangle.setStrokeWidth(2); // Set border width
 
-        //root.getChildren().add(rectangle);
-        Button searchButton = new Button("Search Customer");
-        searchButton.setPrefWidth(200); // Set preferred width to 200 pixels
-        searchButton.setPrefHeight(50);
-        searchButton.setFont(Font.font("Arial", 20));
-        searchButton.setOnAction(event -> {
-            // Handle button click event
-            System.out.println("Search button clicked!");
-            // Implement your search functionality here
-        });
+        // Bind the size of the rectangle to the size of the root BorderPane
+        rectangle.widthProperty().bind(root.widthProperty());
+        rectangle.heightProperty().bind(root.heightProperty());
 
-        Rectangle innerRectangle = new Rectangle();
-        innerRectangle.setWidth(200);
-        innerRectangle.setHeight(50);
-        innerRectangle.setFill(Color.WHITE);
-        innerRectangle.setStroke(Color.BLACK);
-        innerRectangle.setStrokeWidth(1);
-        innerRectangle.setX(rectangle.getX() + 100);
-        innerRectangle.setY(rectangle.getY() + 40);
+        // Create a button
+        Button button = new Button("Search Customer");
+        Font font = Font.font("Arial", FontWeight.BOLD, 16); // Replace with desired font, weight, and size
+        button.setFont(font);
 
-        Text repairITText = new Text("RepairIT");
-        repairITText.setFont(Font.font("Arial", FontWeight.BOLD, 20));
-        repairITText.setX(innerRectangle.getX() + 50);
-        repairITText.setY(innerRectangle.getY() + 30);
+        // Add the rectangle and button to the stack pane
+        stackPane.getChildren().addAll(rectangle, button);
 
-        Rectangle innerBottomRectangle = new Rectangle();
-        innerBottomRectangle.setWidth(rectangle.getWidth() / 2 );
-        innerBottomRectangle.setHeight(rectangle.getHeight() - innerRectangle.getHeight() - 39);
-        innerBottomRectangle.setFill(Color.WHITE);
-        innerBottomRectangle.setStroke(Color.BLACK);
-        innerBottomRectangle.setStrokeWidth(1);
-        innerBottomRectangle.setX(rectangle.getX());
-        innerBottomRectangle.setY(innerRectangle.getY() + innerRectangle.getHeight());
+        // Set the alignment property of the stack pane to position the button in the top right
+        stackPane.setAlignment(button, Pos.TOP_RIGHT );
 
+        // Bind the size of the button to the size of the rectangle
+        button.prefWidthProperty().bind(rectangle.widthProperty().multiply(0.2));
+        button.prefHeightProperty().bind(rectangle.heightProperty().multiply(0.1));
 
-        root.getChildren().add(searchButton);
-
-        Pane pane = new Pane();
-
-        pane.getChildren().addAll(rectangle, searchButton, innerRectangle,repairITText, innerBottomRectangle);
-
-        searchButton.layoutXProperty().bind(rectangle.layoutXProperty().add(rectangle.getWidth()).subtract(searchButton.getWidth() + 150));
-        searchButton.layoutYProperty().bind(rectangle.layoutYProperty().add(90));
-
-        root.setCenter(pane);
+        // Add the stack pane to the root BorderPane
+        root.setCenter(stackPane);
 
         stage.setTitle("RepairIT");
         stage.setScene(scene);
         stage.show();
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 

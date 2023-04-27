693797e1b2c985b2bf690de951927c16a9cbf5c2
cailinjefferson567 <89879396+cailinjefferson567@users.noreply.github.com>
2023-04-27 12:31:34 -0700

    add random

--- source/RepairTicket.java
+++ source/RepairTicket.java
@@ -1,13 +1,64 @@
 public class RepairTicket {
+    public class RepairTicket {
+        private String issue;
+        private String status;
+        private List<String> workflow;
+        int customerID;
+<<<<<<< HEAD
+                Customer customer;
+        Computer computer;
+=======
+        Customer customer;
 
-    int ID;
-     Customer customer;
-     Computer computer;
-    // Workflow[] workflows
-    String status;
-    
-    public RepairTicket() {
-        //todo random unique ID generator
+>>>>>>> origin/main
+        // Workflow[] workflows
+        String status;
+
+        public RepairTicket() {
+            //todo random unique ID generator 8# check to make ure it doesnt exist
+            Set<Integer> idSet = new HashSet<>(); // Set to store generated IDs
+            Random rand = new Random();
+            int customerID;
+
+            do {
+                id = rand.nextInt(90000000) + 10000000; // Generate random 8-digit number
+            } while (!idSet.add(customerID));
+            this.issue = issue;
+            this.status = "Open";
+            this.workflow = new ArrayList<>();
+        }
+
+        public String GetIssue() {
+            return this.issue;
+        }
+
+        public void EditIssue(String newIssue) {
+            this.issue = newIssue;
+        }
+
+        public void AddWorkflow(String workflowStep) {
+            this.workflow.add(workflowStep);
+        }
+
+        public void EditWorkflow(int index, String updatedWorkflowStep) {
+            this.workflow.set(index, updatedWorkflowStep);
+        }
+
+        public void DeleteWorkflow(int index) {
+            this.workflow.remove(index);
+        }
+
+        public String GetStatus() {
+            return this.status;
+        }
+
+        public void Save() {
+            // Code to save the repair ticket to a database or file system
+        }
+
+        public void Close() {
+            this.status = "Closed";
+        }
     }
 
 

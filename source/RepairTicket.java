fcff316427bfebcf3d9f66b41f8dd9793d7c90d2
Justin <justinmolina34@gmail.com>
2023-04-27 17:04:06 -0700

    Repaired repair ticket

--- source/RepairTicket.java
+++ source/RepairTicket.java
@@ -5,64 +5,61 @@ import java.util.Set;
 
 public class RepairTicket {
 
-        private String issue;
-        private String status;
-        private List<String> workflow;
+    private String issue;
+    private String status;
+    private List<String> workflow;
+    int customerID;
+    Customer customer;
+    Computer computer;
+    // Workflow[] workflows
+
+    public RepairTicket() {
+        //todo random unique ID generator 8# check to make ure it doesnt exist
+        Set<Integer> idSet = new HashSet<>(); // Set to store generated IDs
         int customerID;
-        Customer customer;
-        Computer computer;
-        // Workflow[] workflows
 
-        public RepairTicket() {
-            //todo random unique ID generator 8# check to make ure it doesnt exist
-            Set<Integer> idSet = new HashSet<>(); // Set to store generated IDs
-            int customerID;
+        while (true) {
+            // Generate random 8-digit number
+            customerID = (int) (Math.random() * 90000000) + 10000000;
 
-            while (true) {
-                // Generate random 8-digit number
-                customerID = (int)(Math.random() * 90000000) + 10000000;
-
-                if (idSet.add(customerID)) { // Check if ID already exists in set and add it to the set if it's unique
-                    break;
-                }
+            if (idSet.add(customerID)) { // Check if ID already exists in set and add it to the set if it's unique
+                break;
             }
-            this.issue = issue;
-            this.status = "Open";
-            this.workflow = new ArrayList<>();
         }
-
-        public String GetIssue() {
-            return this.issue;
-        }
-
-        public void EditIssue(String newIssue) {
-            this.issue = newIssue;
-        }
-
-        public void AddWorkflow(String workflowStep) {
-            this.workflow.add(workflowStep);
-        }
-
-        public void EditWorkflow(int index, String updatedWorkflowStep) {
-            this.workflow.set(index, updatedWorkflowStep);
-        }
-
-        public void DeleteWorkflow(int index) {
-            this.workflow.remove(index);
-        }
-
-        public String GetStatus() {
-            return this.status;
-        }
-
-        public void Save() {
-            // Code to save the repair ticket to a database or file system
-        }
-
-        public void Close() {
-            this.status = "Closed";
-        }
+        this.issue = issue;
+        this.status = "Open";
+        this.workflow = new ArrayList<>();
     }
 
+    public String GetIssue() {
+        return this.issue;
+    }
 
+    public void EditIssue(String newIssue) {
+        this.issue = newIssue;
+    }
+
+    public void AddWorkflow(String workflowStep) {
+        this.workflow.add(workflowStep);
+    }
+
+    public void EditWorkflow(int index, String updatedWorkflowStep) {
+        this.workflow.set(index, updatedWorkflowStep);
+    }
+
+    public void DeleteWorkflow(int index) {
+        this.workflow.remove(index);
+    }
+
+    public String GetStatus() {
+        return this.status;
+    }
+
+    public void Save() {
+        // Code to save the repair ticket to a database or file system
+    }
+
+    public void Close() {
+        this.status = "Closed";
+    }
 }

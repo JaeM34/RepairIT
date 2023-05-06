2d179b9b4cd492be70eb1a512bef72d977dcb27c
tommyvtran123 <46581856+tommyvtran123@users.noreply.github.com>
2023-05-06 14:00:02 -0700

    added remove button. Repair button now opens a new window

--- source/RepairIT.java
+++ source/RepairIT.java
@@ -6,10 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
@@ -184,10 +181,10 @@ public class RepairIT extends Application {
 
         // Create sample data for the repair queue (replace with your actual data)
         List<Computer> repairList = new ArrayList<>();
-        //repairList.add(new Computer("John", 1));
-        //repairList.add(new Computer("John", 2));
-        //repairList.add(new Computer("John", 3));
-        //repairList.add(new Computer("John", 4));
+        repairList.add(new Computer("John", 1));
+        repairList.add(new Computer("John", 2));
+        repairList.add(new Computer("John", 3));
+        repairList.add(new Computer("John", 4));
 
 
         /// Add repair blocks to the repair queue
@@ -214,10 +211,10 @@ public class RepairIT extends Application {
         Label computersLabel = new Label("Computer's Label: " + customer.getComputers());
         Label repairAllTicketsLabel = new Label("Repair Tickets: " + customer.getRepairTickets());
 
-       // Button removeButton = new Button("Remove");
-       // removeButton.setOnAction(event -> {
-        //    customerBlock.getChildren().remove(customerBlock);
-       // });
+         Button removeButton = new Button("Remove");
+         removeButton.setOnAction(event -> {
+            customerBlock.getChildren().remove(customerBlock);
+         });
 
         // Create a check-in button for the customer
         Button checkInButton = new Button("Check In");
@@ -243,19 +240,47 @@ public class RepairIT extends Application {
         Label computerLabel = new Label("Computer: " + computer.getModel());
         Label computerIDLabel = new Label("Year: " + computer.getYear());
 
+        Button removeButton = new Button("Remove");
+        removeButton.setOnAction(event -> {
+            ((Pane)repairBlock.getParent()).getChildren().remove(repairBlock);
+        });
+
         // Create a repair button for the repair ticket
         Button repairButton = new Button("Repair");
         repairButton.setOnAction(event -> {
-         //    Handle repair action here (e.g., remove repair ticket from repair queue, mark as repaired)
+            //    Handle repair action here (e.g., remove repair ticket from repair queue, mark as repaired)
+            Stage computerInfoStage = new Stage();
+            computerInfoStage.setTitle("OpenedRepairTicketPage");
+
+            // Create a new scene for the computer information
+            VBox layout = new VBox(10);
+
+            // Create UI components for displaying computer information
+            Label computerNameLabel = new Label("Computer Name: " + computer.getModel());
+            Label computerStatusLabel = new Label("Year: " + computer.getYear());
+
+            // Add the UI components to the layout
+            layout.getChildren().addAll(computerNameLabel, computerStatusLabel);
+
+            // Create the scene with the layout as the root
+            Scene computerInfoScene = new Scene(layout);
+
+            computerInfoStage.setScene(computerInfoScene);
+
+            // Set the desired width and height for the window
+            computerInfoStage.setWidth(1000);
+            computerInfoStage.setHeight(600);
+
+            // Show the new window
+            computerInfoStage.show();
+
             System.out.println("Repairing computer: " + computer.getYear() + ", Ticket #" + computer.getYear());
         });
 
         // Add UI components to repair block
-        repairBlock.getChildren().addAll(computerLabel, computerIDLabel, repairButton);
+        repairBlock.getChildren().addAll(computerLabel, computerIDLabel, repairButton, removeButton);
 
         return repairBlock;
     }
-
-
 
 }
